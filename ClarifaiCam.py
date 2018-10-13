330bea3c6d23bfa4a398ec3805642b10d64123fa
Veronica Lin <velin@ucsd.edu>
2018-10-13 12:44:45 -0700

    add photos

--- ClarifaiCam.py
+++ ClarifaiCam.py
@@ -1,59 +1,96 @@
 import cv2 as cv
 import os
-import time
+from threading import Thread
+import clarifai
 from clarifai.rest import ClarifaiApp
-from PyQt5.QtWidgets import QApplication, QMainWindow
 
+class FrameGetter():
+    def __init__(self, src=0, filename="frame.jpg"):
+        self.filename = filename
+        self.cap = cv.VideoCapture(src)
+        (grabbed, self.frame) = self.cap.read()
+        self.stopped = False
 
-app1 = QApplication([])
-win = QMainWindow()
+    def start(self):
+        Thread(target=self.get, args=()).start()
+        return self
 
+    def get(self):
+        while not self.stopped:
+            (grabbed, self.frame) = self.cap.read()
+            cv.imwrite(self.filename, self.frame)
 
-cap = cv.VideoCapture(0)
-app = ClarifaiApp(api_key="6159ef1a7d5d43dcb85ed1cfd7670e4e")
+    def stop(self):
+        self.stopped = True
 
-model = app.public_models.general_model
+class ClarifaiPredict:
+    def __init__(self, filename):
+        self.app = ClarifaiApp(api_key="6159ef1a7d5d43dcb85ed1cfd7670e4e")
+        self.model = self.app.public_models.general_model
+        self.filename = filename
+        self.stopped = False
+        self.concepts = None
 
+    def start(self):
+        Thread(target=self.predict, args=()).start()
+        return self
 
-def captureImageToFile(filename):
-    ret, frame = cap.read()
-    cv.imwrite(filename, frame)
-    return frame
+    def predict(self):
+        while not self.stopped:
+            try:
+                prediction = self.model.predict_by_filename(self.filename)
+            except (clarifai.errors.ApiError):
+                continue
+            self.concepts = prediction["outputs"][0]["data"]["concepts"]
 
-def scale(img):
-    return cv.resize(img, None, fx=2, fy=2, interpolation = cv.INTER_CUBIC)
+    def stop(self):
+        self.stopped = True
 
-def predictImage(filename):
-    return model.predict_by_filename(filename)
+class FrameDrawer():
+    def __init__(self, frame, concepts):
+        self.frame = frame
+        self.stopped = False
 
-def printConcepts(frame, response):
-    concepts = response["outputs"][0]["data"]["concepts"]
-    text = ""
-    for i in range(5):
-        text += concepts[i]['name'] + " "
-    font = cv.FONT_HERSHEY_SIMPLEX
-    cv.putText(frame, text, (50, 50), font, 2, (0, 0, 0), 2)
+    def start(self):
+        Thread(target=self.draw, args=()).start()
+        return self
 
-current_milli_time = lambda: int(round(time.time() * 1000))
+    def draw(self):
+        while not self.stopped:
+            self.img = cv.resize(self.frame, None, fx=2, fy=2)
 
-def update():
-    frame = captureImageToFile("frame.jpg")
-    res = scale(frame)
-    response = predictImage("frame.jpg")
-    printConcepts(res, response)
+            if (self.concepts != None):
+                text = ""
+                for i in range(5):
+                    text += self.concepts[i]['name'] + " "
+                    print(self.concepts[i])
+                font = cv.FONT_HERSHEY_SIMPLEX
+                cv.putText(self.img, text, (50, 50), font, 2, (0, 0, 0), 2)
+                print("")
 
-    cv.imshow("webcam", res)
-    os.remove("frame.jpg")
+            cv.imshow("Display", self.img)
+            if (cv.waitKey(1) == ord("q")):
+                self.stopped = True
 
-lastTime = current_milli_time()
-while (True):
-    if (lastTime + 5000 > current_milli_time()):
-        update()
-        lastTime = current_milli_time()
+    def stop(self):
+        self.stopped = True
 
-    if (cv.waitKey(1) == ord("q")):
-        break
+def main(source=0, filename="frame.jpg"):
+    frameGetter = FrameGetter(source, filename).start()
+    clarifaiPredict = ClarifaiPredict(filename).start()
+    frameDrawer = FrameDrawer(frameGetter.frame, clarifaiPredict.concepts).start()
 
+    while (True):
+        if (frameGetter.stopped or clarifaiPredict.stopped or frameDrawer.stopped):
+            frameGetter.stop()
+            clarifaiPredict.stop()
+            frameDrawer.stop()
+            break
 
+        frameDrawer.frame = frameGetter.frame
+        frameDrawer.concepts = clarifaiPredict.concepts
+
+main()
+
+os.remove("frame.jpg")
 cv.destroyAllWindows()
-app1.exit(app1.exec_())
--- clarafai.py
+++ clarafai.py
@@ -15,8 +15,6 @@ from clarifai.rest import ClarifaiApp
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
-
-
         Dialog.setObjectName("Dialog")
         Dialog.resize(2046, 1367)
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
@@ -24,12 +22,6 @@ class Ui_Dialog(object):
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
-        self.widget = QtWidgets.QWidget(Dialog)
-        self.widget.setGeometry(QtCore.QRect(-430, -470, 1931, 1001))
-        self.widget.setObjectName("widget")
-        self.openGLWidget = QtWidgets.QOpenGLWidget(self.widget)
-        self.openGLWidget.setGeometry(QtCore.QRect(500, 640, 861, 861))
-        self.openGLWidget.setObjectName("openGLWidget")
         self.gridLayoutWidget = QtWidgets.QWidget(Dialog)
         self.gridLayoutWidget.setGeometry(QtCore.QRect(1020, 100, 971, 981))
         self.gridLayoutWidget.setObjectName("gridLayoutWidget")
@@ -38,6 +30,7 @@ class Ui_Dialog(object):
         self.gridLayout.setObjectName("gridLayout")
 
         self.label = QtWidgets.QLabel(self.gridLayoutWidget)
+        self.label.setObjectName("label")
         self.gridLayout.addWidget(self.label, 1, 1, 1, 1)
 
         self.label_2 = QtWidgets.QLabel(self.gridLayoutWidget)
@@ -64,6 +57,10 @@ class Ui_Dialog(object):
         self.label_7.setGeometry(QtCore.QRect(190, 1150, 1781, 81))
         self.label_7.setObjectName("label_7")
 
+        self.label_8 = QtWidgets.QLabel(Dialog)
+        self.label_8.setGeometry(QtCore.QRect(140, 240, 691, 701))
+        self.label_8.setObjectName("label_8")
+
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
         self.buttonBox.rejected.connect(Dialog.reject)
@@ -71,17 +68,41 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        
-        self.label.setText(_translate("Dialog", "Image2"))
-        self.label_2.setText(_translate("Dialog", "Image2"))
-        self.label_4.setText(_translate("Dialog", "Image1"))
-        self.label_3.setText(_translate("Dialog", "Image3"))
-        self.label_5.setText(_translate("Dialog", "Image5"))
-        self.label_6.setText(_translate("Dialog", "Image6"))
+
+        image = QPixmap("scene.jpg")
+        image = image.scaledToWidth(300)
+        image = image.scaledToHeight(300)
+        self.label.setPixmap(image)
+
+        image2 = QPixmap("scene.jpg")
+        image2 = image2.scaledToWidth(300)
+        image2 = image2.scaledToHeight(300)
+        self.label_2.setPixmap(image2)
+
+        image3 = QPixmap("scene.jpg")
+        image3 = image3.scaledToWidth(300)
+        image3 = image3.scaledToHeight(300)
+        self.label_3.setPixmap(image3)
+
+        image4 = QPixmap("scene.jpg")
+        image4 = image4.scaledToWidth(300)
+        image4 = image4.scaledToHeight(300)
+        self.label_4.setPixmap(image4)
+
+        image5 = QPixmap("scene.jpg")
+        image5 = image5.scaledToWidth(300)
+        image5 = image5.scaledToHeight(300)
+        self.label_5.setPixmap(image5)
+
+
+        image6 = QPixmap("scene.jpg")
+        image6 = image6.scaledToWidth(300)
+        image6 = image6.scaledToHeight(300)
+        self.label_6.setPixmap(image6)
 
         self.label_7.setText(_translate("Dialog", "Top Searches"))
+        self.label_8.setText(_translate("Dialog", "SCRNSHT"))
 
 
 if __name__ == "__main__":
