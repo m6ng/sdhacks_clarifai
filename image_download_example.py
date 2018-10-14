fd42061d14d5c870eea8f68ba89348de94600f02
m6ng <m6ng@ucsd.edu>
2018-10-13 17:39:58 -0700

    API example provided

---
+++ image_download_example.py
@@ -0,0 +1,9 @@
+# this file contains code to insert into the main python script later.
+
+# This is a python script that can search and download hundreds of 'Google Images."
+# Github link: https://github.com/hardikvasa/google-images-download
+
+from google-images-download import google_images_download
+
+#example - keywords are apple, person, and birds, with a limit of 10 image downloads per keyword
+googleimagesdownload -k "apple, person, birds" -l 10
